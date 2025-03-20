fc7e8267fae784b3a8fdb9ae16e212712d9578f0
Brahim TAHIRI <pro.tahiri.brahim@gmail.com>
2025-03-20 11:57:49 +0100

    Changement du port de l'application.

--- __init__.py
+++ __init__.py
@@ -91,4 +91,5 @@ def page_not_found(e):
     return render_template('/errors/404.html'), 404 
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", port = 80 , debug = True)
+    # app.run(host = "0.0.0.0", port = 80 , debug = True)
+    app.run(debug = True) 
