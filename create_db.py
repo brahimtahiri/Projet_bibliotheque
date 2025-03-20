afa8a56f122cce3aa278a8c141ad3c9e5384a354
Brahim TAHIRI <pro.tahiri.brahim@gmail.com>
2025-03-20 13:07:22 +0100

    Ajout d'une image en arrière-plan dans la page de connexion et mise à jour des fichiers associés. - Modifications de la base de données et du script `create_db.py`. - Ajout du fichier image `background.jpeg` dans le répertoire `static/img/`. - Mise à jour du fichier CSS `styles.css` pour l'intégration de l'image d'arrière-plan. - Modifications de la structure de la page dans `base.html` et `index.html` pour appliquer l'image en arrière-plan dans la page de connexion.

--- create_db.py
+++ create_db.py
@@ -1,36 +1,47 @@
 import sqlite3
 
-connection = sqlite3.connect('database.db')
+DATABASE_NAME = "database.db"
+
+connection = sqlite3.connect(DATABASE_NAME)
 cursor = connection.cursor()
 
-with open('schema.sql') as file:
-    connection.executescript(file.read())
+try:
+    with open('schema.sql') as file:
+        connection.executescript(file.read())
 
-books = [
-    (1, 'Le Seigneur des Anneaux', 'J.R.R. Tolkien', 1954, 'Fantasy', True, 10),
-    (2, '1984', 'George Orwell', 1949, 'Dystopie', True, 8),
-    (3, 'Harry Potter à l\'école des sorciers', 'J.K. Rowling', 1997, 'Fantasy', True, 15),
-    (4, 'L\'Alchimiste', 'Paulo Coelho', 1988, 'Roman initiatique', True, 12),
-    (5, 'Les Misérables', 'Victor Hugo', 1862, 'Classique', True, 5),
-    (6, 'L\'Art de la guerre', 'Sun Tzu', -500, 'Philosophie', True, 7),
-    (7, 'Le Petit Prince', 'Antoine de Saint-Exupéry', 1943, 'Conte', True, 20),
-    (8, 'Don Quichotte', 'Miguel de Cervantes', 1605, 'Classique', True, 6),
-    (9, 'La Peste', 'Albert Camus', 1947, 'Roman existentialiste', True, 9),
-    (10, 'Fahrenheit 451', 'Ray Bradbury', 1953, 'Science-fiction', True, 11)
-]
+    print("Base de données créée avec succès.")
+except:
+    print("Une erreur est survenue lors de la création de la base de données...")
 
-cursor.executemany('''INSERT INTO Livres (id_livre, titre, auteur, annee_publication, genre, disponible, quantite_stock)
-           VALUES (?, ?, ?, ?, ?, ?, ?)''', books)
+try:
+    books = [
+        (1, 'Le Seigneur des Anneaux', 'J.R.R. Tolkien', 1954, 'Fantasy', True, 10),
+        (2, '1984', 'George Orwell', 1949, 'Dystopie', True, 8),
+        (3, 'Harry Potter à l\'école des sorciers', 'J.K. Rowling', 1997, 'Fantasy', True, 15),
+        (4, 'L\'Alchimiste', 'Paulo Coelho', 1988, 'Roman initiatique', True, 12),
+        (5, 'Les Misérables', 'Victor Hugo', 1862, 'Classique', True, 5),
+        (6, 'L\'Art de la guerre', 'Sun Tzu', -500, 'Philosophie', True, 7),
+        (7, 'Le Petit Prince', 'Antoine de Saint-Exupéry', 1943, 'Conte', True, 20),
+        (8, 'Don Quichotte', 'Miguel de Cervantes', 1605, 'Classique', True, 6),
+        (9, 'La Peste', 'Albert Camus', 1947, 'Roman existentialiste', True, 9),
+        (10, 'Fahrenheit 451', 'Ray Bradbury', 1953, 'Science-fiction', True, 11)
+    ]
 
-users = [
-    (0, "Administrateur", "", "administrateur", "administrateur", "administrator"),
-    (1, 'Martin', 'Claire', 'cmartin', "cmartin", 'librarian'),
-    (2, 'Dupont', 'Jean', 'jdupont', "jdupont", 'member'),
-]
+    cursor.executemany('''INSERT INTO Livres (id_livre, titre, auteur, annee_publication, genre, disponible, quantite_stock)
+            VALUES (?, ?, ?, ?, ?, ?, ?)''', books)
 
-cursor.executemany('''INSERT INTO Utilisateurs (id_utilisateur, nom, prenom, username, password, type_utilisateur)
-                      VALUES (?, ?, ?, ?, ?, ?)''', users)
+    users = [
+        (0, "Administrateur", "", "administrateur", "administrateur", "administrator"),
+        (1, 'Martin', 'Claire', 'cmartin', "cmartin", 'librarian'),
+        (2, 'Dupont', 'Jean', 'jdupont', "jdupont", 'member'),
+    ]
 
+    cursor.executemany('''INSERT INTO Utilisateurs (id_utilisateur, nom, prenom, username, password, type_utilisateur)
+                        VALUES (?, ?, ?, ?, ?, ?)''', users)
+    
+    print("Les données ont été migrées avec succès.\nLa base de données est disponible sous le nom de '%s'" %DATABASE_NAME)
+except:
+    print("Une erreur est survenue lors de la migration des données...",)
 
 connection.commit()
 connection.close()
